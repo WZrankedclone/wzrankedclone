49e56bf39a9a49c923c46db8cc5c294e76dc7ad4
Randy Hyun <randyh5478@gmail.com>
2021-07-29 13:16:07 -0500

    Debugging:debugging data

--- src/mainPageStore.js
+++ src/mainPageStore.js
@@ -1,17 +1,39 @@
 // ACTION TYPES
 const SET_SEARCH = "SET_SEARCH";
 const SET_TOGGLE = "SET_TOGGLE";
+const GET_STATS = "GET_STATS";
 
 // ACTION CREATORS - function that changes state
-export const setSearch = (sVal) => ({
-  type: SET_SEARCH,
-  sVal,
-});
+// export const setSearch = (sVal) => ({
+//   type: SET_SEARCH,
+//   sVal,
+// });
+
+export const setSearch = (sVal) => {
+  console.log("ACTION SEARCH OCCURED");
+  return {  
+    type: SET_SEARCH,
+    sVal,}
+}
 
 export const setToggle = (pVal) => ({
   type: SET_TOGGLE,
   pVal,
 });
+
+// export const getStats = (statsZ) => ({
+//   type: GET_STATS,
+//   statsZ,
+// });
+
+export const getStats = (statsZ) => {
+  console.log("getStats ACTION OCCURED", statsZ)
+  return {
+    type: GET_STATS,
+    statsZ,
+  }
+}
+
 
 // THUNK CREATORS
 
@@ -19,12 +41,14 @@ export const setToggle = (pVal) => ({
 const initialState = {
   searchValue: "",
   toggleValue: "",
+  THEstats: {}
 };
 
 // REDUCER
 export default function (state = initialState, action) {
   switch (action.type) {
     case SET_SEARCH:
+      console.log("SET_SEARCH REDUCER OCCURED") 
       return {
         ...state,
         searchValue: action.sVal,
@@ -34,6 +58,12 @@ export default function (state = initialState, action) {
         ...state,
         toggleValue: action.pVal,
       };
+      case GET_STATS:
+        console.log("GET_STATS REDUCER OCCURED") 
+        return {
+          ...state,
+          THEstats: action.statsZ,
+        };
     default:
       return state;
   }
